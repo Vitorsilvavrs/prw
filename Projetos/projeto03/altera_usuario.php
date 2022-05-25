d4bfda6e0941e89f2149f7775233d235731b27e3
vitin <rodrigues.vitor1@aluno.ifsp.edu.br>
2022-05-25 10:22:12 -0300

    Projeto 03

---
+++ Projetos/projeto03/altera_usuario.php
@@ -0,0 +1,48 @@
+<?php
+     include('conexao.php');
+     $id_usuario=$_GET['id_usuario'];
+     $sql='SELECT * FROM usuario where id_usuario ='. $id_usuario;
+     $result=mysqli_query($con, $sql);
+     $row= mysqli_fetch_array($result);
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de usuário</title>
+    <h1>Alteração de usuário</h1>
+        <form method="post" action="altera_usuario_exe.php">
+        <table>
+            <tr>
+                <th colspan="2">Cadastro de Clientes - IFSP</th>
+            </tr>
+            <tr>
+                <td><p>Nome: </p></td>
+                <td><input type="text" class="inpTxt" name="nome_usuario" maxlength="50" value="<?php echo $row['nome_usuario'] ?>"></td>
+            </tr>
+            <tr>
+                <td><p>E-mail: </p></td>
+                <td><input type="text" class="inpTxt" name="email_usuario" maxlength="50" value="<?php echo $row['email_usuario'] ?>"> </td>
+
+            </tr>
+            <tr>
+                <td><p>Telefone: </p></td>
+                <td><input type= "text" class="inpTxt inpTel" name="telefone_usuario" maxlength="30"value="<?php echo $row['telefone_usuario'] ?>"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><br><input type="submit" class="btnEnviar" value="Enviar"></td>
+            </tr>
+        </table>
+        <input name="id_usuario" type="hidden" value="<?php echo $row['id_usuario']?>">   
+        
+    </form>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ Projetos/projeto03/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Menu Principal</title>
+</head>
+<body>
+    <h1>Menu principal</h1>
+    <div>
+    <ul>
+        <li><a href="cadastro_usuario.html">Cadastrar usuário</a></li>
+        <li><a href="listar_usuario.php">Listar usuário</a></li>
+    </ul>
+    </div>
+</body>
+</html>
